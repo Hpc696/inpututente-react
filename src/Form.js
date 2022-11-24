0816928b03da3f513a73326e8ec0652d922f7ed7
MatteoG <honrop.giochi@gmail.com>
2022-11-24 10:17:49 +0100

    input funzionante con useRef, useState

--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,17 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
+
 export default function Form(){
+    
     const nome= useRef('nome')
+
+    const [mostra, setMostra] = useState("")
+
     function stampanome(event){
+
         event.preventDefault()
         console.log(nome.current.value)
+        setMostra(nome.current.value)
+    
     }
     return(
         <>
@@ -12,7 +20,7 @@ export default function Form(){
                 <input ref={nome} type='text'/>
                 <button onClick={stampanome}>INVIOO</button>
             </form>
-            <div>{nome.current.value}</div>
+            <div>Ti chiami {mostra}</div>
         </>
     )
 }
